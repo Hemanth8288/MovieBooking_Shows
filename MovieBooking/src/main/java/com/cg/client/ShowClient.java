7b0cce5d0c5dcf42670f414f1ad1817d1043293a
lenovo <lenovo@DESKTOP-I7EESSH>
2020-03-23 12:50:17 +0530

    second commit removed single letter variables

--- MovieBooking/src/main/java/com/cg/client/ShowClient.java
+++ MovieBooking/src/main/java/com/cg/client/ShowClient.java
@@ -4,10 +4,10 @@ import com.cg.exception.MovieException;
 import com.cg.service.ShowServiceImpl;
 
 public class ShowClient {
-	public static void main(String[] args) throws MovieException{
+	public static void main(String[] args) throws MovieException {
 		ShowServiceImpl ser = new ShowServiceImpl();
-	ser.getShows(2222, "NightShow").forEach(System.out::println);
-		//System.out.println(ser.getShows(1111, "NoonShow"));
+		ser.getShows(2222, "NightShow").forEach(System.out::println);
+
 	}
 
 }
