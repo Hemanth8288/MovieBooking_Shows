4c10e0e5c35b0f1cd357f58db7f9e8c49e63a624
lenovo <lenovo@DESKTOP-I7EESSH>
2020-03-23 15:25:15 +0530

    Added test cases

--- MovieBooking/src/test/java/com/cg/MovieBooking/MovieBookingTest.java
+++ MovieBooking/src/test/java/com/cg/MovieBooking/MovieBookingTest.java
@@ -44,51 +44,52 @@ public class MovieBookingTest {
 		ShowService ser = new ShowServiceImpl();
 		assertThrows(MovieException.class, () -> ser.getShows(0));
 	}
+
 	@Test
 	@DisplayName("TC_MOVIE_Ser_03")
 	public void movieBookingTest5() {
 		ShowService ser = new ShowServiceImpl();
 		assertThrows(MovieException.class, () -> ser.getShows(1112));
 	}
+
 	@Test
 	@DisplayName("TC_MOVIE_Dao_01")
 	public void movieBookingTest6() {
 		ShowDao ser = new ShowDaoImpl();
 		assertThrows(MovieException.class, () -> ser.getShows(110011001));
 	}
+
 	@Test
 	@DisplayName("TC_MOVIE_Dao_02")
 	public void movieBookingTest7() {
 		ShowDao ser = new ShowDaoImpl();
 		assertThrows(MovieException.class, () -> ser.getShows(0));
 	}
+
 	@Test
 	@DisplayName("TC_MOVIE_Dao_03")
 	public void movieBookingTest8() {
 		ShowDao ser = new ShowDaoImpl();
 		assertThrows(MovieException.class, () -> ser.getTheatre(2223));
 	}
+
 	@Test
 	@DisplayName("TC_MOVIE_Dao_04")
 	public void movieBookingTest9() {
 		ShowDao ser = new ShowDaoImpl();
 		assertThrows(MovieException.class, () -> ser.getShows(11001101));
 	}
+
 	@Test
 	@DisplayName("TC_MOVIE_Dao_05")
 	public void movieBookingTest10() {
 		ShowDao ser = new ShowDaoImpl();
 		assertThrows(MovieException.class, () -> ser.getShows(0));
 	}
+
 	@Test
 	@DisplayName("TC_MOVIE_Dao_06")
 	public void movieBookingTest11() {
-		ShowDao ser = new ShowDaoImpl();
-		assertThrows(MovieException.class, () -> ser.getShows(1113));
-	}
-	@Test
-	@DisplayName("TC_MOVIE_UI_01")
-	public void movieBookingTest12() {
 		ShowDao ser = new ShowDaoImpl();
 		assertThrows(MovieException.class, () -> ser.getShows(1113));
 	}
